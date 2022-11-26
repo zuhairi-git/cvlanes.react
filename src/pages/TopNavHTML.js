33bf2a5c08d293e51f3335bd8fdb60a780cc5c82
Ali Al-Zuhairi <zohairi@live.com>
2022-11-26 13:18:16 +0200

    add react touter dom

--- src/pages/TopNavHTML.js
+++ src/pages/TopNavHTML.js
@@ -3,26 +3,26 @@ import React from 'react';
 class TopNavHTML extends React.Component {
   render() {
     return (
-      <div class="navbar nav navbar-expand-lg bg-light navbar-light" id="navbar">
-        <div class="container-fluid">
-          <a href="index.html" class="navbar-brand"><img src="img/CV-Lane-h.png" alt="Logo" /></a>
+      <div className="navbar nav navbar-expand-lg bg-light navbar-light" id="navbar">
+        <div className="container-fluid">
+          <a href="index.html" className="navbar-brand"><img src="img/CV-Lane-h.png" alt="Logo" /></a>
 
-          <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
+          <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
-            <div class="navbar-nav ml-auto">
-              <a href="#home" class="nav-item nav-link active">Home</a>
-              <a href="#about" class="nav-item nav-link">About</a>
-              <a href="#service" class="d-none nav-item nav-link">Service</a>
-              <a href="#experience" class="nav-item nav-link">Experience</a>
-              <a href="#achievements" class="nav-item nav-link">Achievements</a>
-              <a href="#price" class="d-none nav-item nav-link">Price</a>
-              <a href="#team" class="nav-item nav-link">Social Media</a>
-              <a href="#review" class="nav-item nav-link">Recommendations</a>
-              <a href="#blog" class="d-none nav-item nav-link">Blog</a>
-              <a href="#footer" class="nav-item nav-link">Contact</a>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarCollapse">
+            <div className="navbar-nav ml-auto">
+              <a href="#home" className="nav-item nav-link active">Home</a>
+              <a href="#about" className="nav-item nav-link">About</a>
+              <a href="#service" className="d-none nav-item nav-link">Service</a>
+              <a href="#experience" className="nav-item nav-link">Experience</a>
+              <a href="#achievements" className="nav-item nav-link">Achievements</a>
+              <a href="#price" className="d-none nav-item nav-link">Price</a>
+              <a href="#team" className="nav-item nav-link">Social Media</a>
+              <a href="#review" className="nav-item nav-link">Recommendations</a>
+              <a href="#blog" className="d-none nav-item nav-link">Blog</a>
+              <a href="#footer" className="nav-item nav-link">Contact</a>
             </div>
           </div>
           
