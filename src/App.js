3bf6eb95733d9814b38409b4122700d7602963f7
Ali Al-Zuhairi <zohairi@live.com>
2023-01-23 11:30:23 +0200

    redesigning the footer

--- src/App.js
+++ src/App.js
@@ -347,25 +347,25 @@ class App extends React.Component {
                     <div className="container-fluid">
                         <div className="container">
                             <div className="row">
-                                <div className="col-md-4 col-xs-12 mb-5">
+                                <div className="col-md-4 col-xs-12">
                                     <div className="footer-info">
                                         <div className="footer-social">
-                                            <img src={require("./img/cvlanes-bg.png")} style={{ width: '256px', marginTop: '-40px' }} alt="Logo" />
+                                            <img src={require("./img/cvlanes-bg.png")} style={{ height: '256px' }} alt="Logo" />
                                         </div>
                                     </div>
                                 </div>
-                                <div className="col-md-4 col-xs-12  mb-5">
+                                <div className="col-md-4 col-xs-12 align-items-center text-center">
                                     <div className="footer-info">
-                                        <h2>Ali Zuhairi</h2>
-                                        <h3>Espoo, Suurpelto</h3>
-                                        <h3>044 2 844 844</h3>
-                                        <h3>zohairi@live.com</h3>
+                                        <img src={DayOfMylife} alt="Ali" style={{ height: '230px' }} />
+                                        <h3>A Day Of My Life</h3>
                                     </div>
                                 </div>
-                                <div className="col-md-4 col-xs-12  align-items-center text-center">
-                                    <div className="footer-info">
-                                        <h2>A Day Of My Life</h2>
-                                        <img src={DayOfMylife} alt="Ali" width={'250px'} />
+                                <div className="col-md-4 col-xs-12 mb-2">
+                                    <div className="footer-info hidden-sm hidden-xs hidden-xxs">
+                                        <p className='mt-3 lh-lg fs-5 text-start ps-3 text-white border-start border-2'><b>Ali Zuhairi</b> <br /> Espoo, Suurpelto <br /> 044 2 844 844 <br /> zohairi@live.com</p>
+                                    </div>
+                                    <div className="footer-info hidden-md hidden-lg hidden-xl hidden-xxl copyright">
+                                        <p className='mt-3 lh-lg fs-5 text-center ps-3 text-white'><b>Ali Zuhairi</b> <br /> Espoo, Suurpelto <br /> 044 2 844 844 <br /> zohairi@live.com</p>
                                     </div>
                                 </div>
                             </div>
