9bd33877961f658d4cd9fa164ea6967908886d26
Ali Al-Zuhairi <zohairi@live.com>
2023-03-28 14:22:48 +0300

    Update Portfolio.js

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -38,7 +38,7 @@ class App extends React.Component {
                 <div className="container-fluid">
                     <h2 className='text-center mt-5 primary-title display-2'>Portfolio</h2>
                     <div className="row p-5">
-                        <a href='/portfolio/Jobseeking'>
+                        <a href='/portfolio/jobseeking'>
                         <div className="col-sm-12 col-md-4 wow fadeIn" data-wow-delay="0.2s">
                             <img src='../img/portfolio-cover-dark.jpeg' className="w-100" alt="cover" />
                             <h5 className='mt-2'>Job Seeking</h5>
