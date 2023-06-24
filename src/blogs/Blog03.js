2258f204fb7d6f3c229750776c72ff00dda6a2b6
Ali Al-Zuhairi <zohairi@live.com>
2023-06-24 17:34:24 +0300

    adding more to the blog

--- src/blogs/Blog03.js
+++ src/blogs/Blog03.js
@@ -11,8 +11,6 @@ import Footer from '../pages/Footer';
 import BlogAuthor from './BlogAuthor';
 import BlogShare from './BlogShare';
 import { Helmet } from "react-helmet";
-import YoutubeShortsEmbed from "../generic/YoutubeShortsEmbed";
-import { TikTok } from "react-tiktok";
 
 class App extends React.Component {
     componentDidMount() {
@@ -56,17 +54,24 @@ class App extends React.Component {
                     <div className='blog-width m-5 mx-auto'>
                         <h4 className='primary-title'>Embracing the Era of AI: Humanity's Journey to Unparalleled Sophistication</h4>
                         <BlogAuthor />
-                        <p className='mb-4'>
-                            I have a strong feeling that with the progress of AI and the advancements in machine learning, our world will reach such a level of sophistication that humans may appear primitive once more.                        </p>
+                        <div className='quoteContainer mb-5'>
+                            <q className='mb-4'>
+                                I have a feeling that with the progress of AI and the advancements in machine learning, our world will reach such a level of sophistication that most humans may appear <span className='quoteHighlight'>primitive once more.</span>
+                            </q>
+                            <p className='quoteLine'>
+                                Ali
+                            </p>
+                        </div>
+
+                        <h4>Introduction:</h4>
+                        <p>
+                            In an era where technological advancements are reshaping our world at an unprecedented pace, it is not uncommon for individuals to ponder the future implications of such progress. Among these advancements, the rise of Artificial Intelligence (AI) and Machine Learning (ML) has been particularly transformative. This article explores the belief that our world is headed towards an extraordinary level of sophistication, potentially rendering humans primitive in comparison.
+                        </p>
                         <div className='text-center mb-5 mt-5'>
                             <div className='bg-image'>
                                 <img className='w-100' src='../img/blog/Human-AI/human-VR.webp' style={{ width: '100%' }} alt='Image' />
                             </div>
                         </div>
-                        <h4>Introduction:</h4>
-                        <p>
-                            In an era where technological advancements are reshaping our world at an unprecedented pace, it is not uncommon for individuals to ponder the future implications of such progress. Among these advancements, the rise of Artificial Intelligence (AI) and Machine Learning (ML) has been particularly transformative. This article explores the belief that our world is headed towards an extraordinary level of sophistication, potentially rendering humans primitive in comparison.
-                        </p>
 
                         <h4>The Accelerating Power of AI and ML:</h4>
                         <p>
@@ -80,14 +85,14 @@ class App extends React.Component {
 
                         <h4>The Potential for Human Primitiveness:</h4>
                         <p>
-                            As AI and ML progress towards unprecedented sophistication, there arises a compelling argument that humans may appear primitive in comparison. This perspective stems from the notion that the fusion of AI with advanced robotics, nanotechnology, and other emerging fields could result in machines surpassing human intelligence and physical capabilities. While it is crucial to approach this idea with caution, contemplating the potential outcomes is both intriguing and thought-provoking.
+                            As AI and ML progress towards unprecedented sophistication, there arises a compelling argument that most humans may appear primitive in comparison. This perspective stems from the notion that the fusion of AI with advanced robotics, nanotechnology, and other emerging fields could result in machines surpassing human intelligence and physical capabilities. While it is crucial to approach this idea with caution, contemplating the potential outcomes is both intriguing and thought-provoking.
                         </p>
 
                         <h4>Beyond the Boundaries of Human Limitations:</h4>
                         <p>
                             One of the primary drivers behind this belief is the limitations inherent in the human condition. Despite our remarkable achievements, humans are constrained by biological factors, including our cognitive capacities, lifespan, and susceptibility to errors. In contrast, AI systems have the potential to transcend these limitations, offering the ability to process and analyze vast amounts of information rapidly, make unbiased decisions, and continuously improve without succumbing to fatigue or emotional biases.
                         </p>
-<h4></h4>
+                        <h4></h4>
                         <h4>A Synergistic Future:</h4>
                         <p>
                             Rather than envisioning a dystopian future where humans are rendered obsolete, it is essential to emphasize the potential for a harmonious coexistence between humans and AI. As AI advances, it can become an invaluable tool to augment human intelligence and creativity. By leveraging the unique strengths of both humans and machines, we can solve complex problems, explore new frontiers of knowledge, and unlock unimagined possibilities.
@@ -103,14 +108,32 @@ class App extends React.Component {
                             The belief that AI and ML advancements will lead our world to unparalleled sophistication, potentially rendering humans primitive in comparison, is a thought-provoking concept. As AI continues to evolve, it holds immense potential to transform society, industries, and our very notion of what it means to be human. It is crucial to approach this journey with a balanced perspective, embracing the opportunities that arise while ensuring that human values and ethics guide our path forward. By doing so, we can shape a future where humans and AI coexist symbiotically, harnessing the vast potential of both to create a world that transcends our current limitations.
                         </p>
 
-                        <h4>Are you still uncertain or sceptical?</h4>
+                        <h4>After Conclusion</h4>
                         <p>
-                            In case you are still uncertain or sceptical, it is important to note that the article thus far has been created by an AI, utilizing my statement:
-                            "I have a strong feeling that with the progress of AI and the advancements in machine learning, our world will reach such a level of sophistication that humans may appear primitive once more." I must admit, it is quite remarkable.
+                            In case you are still uncertain or sceptical, it is important to note that the article thus far has been created by an AI, utilizing my statement: <span className='quoteHighlightLight'>"I have a feeling that with the progress of AI and the advancements in machine learning, our world will reach such a level of sophistication that most humans may appear primitive once more."</span> I must admit, it is quite remarkable.
                         </p>
                         <p>
-                            The inception of this idea was not driven by me being against AI or ML, but rather by a profound belief in the significance of expanding our understanding of the ever-changing world and redefining educational principles. We stand at a pivotal moment in history, where the path we tread holds the potential for irreversible consequences. This demands a profound change in our approach to education and the work environment. As I often emphasize, the focus should not solely be on enhancing the intelligence of our surroundings. Instead, we ought to explore ways to enhance human capabilities and embrace the enhancement of our creativity.
-
+                            The inception of this idea behind this article was not driven by me being against AI, but rather by a profound belief in the significance of expanding our understanding of the ever-changing world and redefining educational principles. We stand at a pivotal moment in history, where the path we tread holds the potential for irreversible consequences. This demands a profound change in our approach to education and the work environment. As I often emphasize, the focus should not solely be on enhancing the intelligence of our surroundings. Instead, we ought to explore ways to enhance human capabilities and embrace the enhancement of our creativity.
+                        </p>
+                        <p>
+                            To accomplish this, in my opinion, there are three key points to consider:
+                            <ol>
+                                <li>
+                                    <span className='quoteHighlight'>Know-what:</span> It is crucial to understand what AI is and how developers interact with it at a code level. This entails gaining knowledge about the inner workings of AI systems.
+                                </li>
+                                <li>
+                                    <span className='quoteHighlight'>Know-why:</span> We must strive to reach a collective understanding and, ideally, an agreement on why we need AI. This involves recognizing the benefits and potential risks associated with its implementation.
+                                </li>
+                                <li>
+                                    <span className='quoteHighlight'>Know-how:</span> By mastering the first two points, we can begin to establish regulations or boundaries that are universally understood and accepted.
+                                </li>
+                            </ol>
+                        </p>
+                        <h4>
+                            There is more...
+                        </h4>
+                        <p>
+                            Having discussed the significance of AI, it is worth noting that there is already an ideation of Artificial General Intelligence (AGI). AGI refers to a machine that can learn and comprehend any intellectual task that a human being can, and can even develop capabilities beyond the scope of traditional AI systems.
                         </p>
                         <BlogShare />
                     </div>
